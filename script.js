798720de0801b084caed164d1eb615c692b21f0c
Julia B <julia.bernst@gmail.com>
2023-02-06 18:27:27 -0500

    Added code to display forecast. Code doesn't work because of bad latitude, but i'm not sure what to do about that and I would like this to be over

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-var search = document.querySelector("#search");
-var searchBtn = document.querySelector("#searchBtn");
-var main = document.querySelector(".main");
+var searchBtn = $("#searchBtn");
+var mainContainer = $("#main");
+var forecastContainer = $("#forecast");
 
 var APIKey = "75057e7bd9899dd0671687e4e687b5b6";
-var today = dayjs().format("D/MM/YYYY");
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
 
 function getLocationAPI(city) {
   var geoQueryURL =
@@ -37,23 +42,34 @@ function getWeatherAPI(lat, lon, cName) {
     "&lon=" +
     lon +
     "&appid=" +
-    APIKey;
+    APIKey +
+    "&units=imperial";
   fetch(weatherQueryURL).then(function (response) {
     response.json().then(function (data) {
-      main.firstChild.textContent = cName + " " + today + data.weather.icon;
+      // Clear children from main
+      removeAllChildNodes(mainContainer);
 
-      main.lastChild.textContent =
+      // Create a card with the weather today
+      var mainCard = document.createElement("div");
+
+      var mainTitle = document.createElement("h2");
+      mainTitle.textContent = cName + " " + dayjs(data.dt) + data.weather.icon;
+
+      var mainWeather = document.createElement("p");
+      mainWeather.textContent =
         "Temp: " +
-        (data.main.temp - 273.15) +
-        "C\n" +
+        data.main.temp +
+        "F\n" +
         "Wind: " +
-        data.wind.speed.imperial +
+        data.wind.speed +
         " MPH\n" +
         "Humidity: " +
         data.main.humidity +
         " %";
 
-      // Turn the card on
+      mainCard.append(mainTitle, mainWeather);
+      mainContainer.appendChild(mainCard);
+      //mainCard.addClass("card");
 
       getForecastAPI(cName);
     });
@@ -63,26 +79,53 @@ function getWeatherAPI(lat, lon, cName) {
 //* Use forecast API to display the 5-Day Forecast
 function getForecastAPI(name) {
   var forecastQueryURL =
-    "api.openweathermap.org/data/2.5/forecast?q=" + name + "&appid=" + APIKey;
-  fetch(forecastQueryURL).then(function (response) {});
+    "api.openweathermap.org/data/2.5/forecast?q=" +
+    name +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+  fetch(forecastQueryURL).then(function (response) {
+    response.json().then(function (data) {
+      // Remove existing children
+      removeAllChildNodes(forecastContainer);
+
+      // Create cards for the 5 day forecast
+      for (var i = 0; i < 5; i++) {
+        var card = document.createElement("div");
+        //card.addClass("card");
+        //card.addClass("text-bg-dark");
+
+        var cardTitle = document.createElement("h2");
+        cardTitle.textContent =
+          dayjs(data.list[i].dt).format("D/MM/YYYY") +
+          data.list[i].weather.icon;
+
+        var cardWeather = document.createElement("p");
+        cardWeather.textContent =
+          "Temp: " +
+          (data.list[i].main.temp - 273.15) +
+          "F\n" +
+          "Wind: " +
+          data.list[i].wind.speed +
+          " MPH\n" +
+          "Humidity: " +
+          data.list[i].main.humidity +
+          " %";
+
+        card.append(cardTitle, cardWeather);
+        forecastContainer.appendChild(card);
+      }
+    });
+  });
 }
 
-searchBtn.addEventListener("click", function () {
-  var inputCity = search.value();
+searchBtn.on("click", function (event) {
+  event.preventDefault();
+
+  var inputCity = $("#search").val();
 
   if (inputCity) {
     getLocationAPI(inputCity);
   }
+  // Set recent searches
 });
-
-//get the city name
-
-// create an event listener for the search button
-// when the button is clicked, check that they entered a real city
-// then:
-// use the city name to change the main-fc to have
-// city name and date
-// temp, wind, humidity
-// use the city to change the 5-Day Forecast
-// loop through the children, change each top text to date
-// and apply the information
